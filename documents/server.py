4575f83188cca4af13ed4b68e3d4056d8cf3c7a3
YumYummity <103061664+YumYummity@users.noreply.github.com>
2024-07-16 19:22:12 -0400

    Replace config.json.... again

--- documents/server.py
+++ documents/server.py
@@ -1,15 +1,27 @@
 # Import types
 from beanie import Document
 from typing import Optional
+from pydantic import BaseModel
 
+class logging_channels(BaseModel):
+    ...
 
 # Define the server document
 class Server(Document):
     """
     - serverId - `str` - The server's Id
     - prefix - `str`, `None` - The server's prefix. (Defaults to None)
+    - logging - `dict` - Logging channels for events. (Defaults to {})
+    - members - `dict` - Members data and punishment log. (Defaults to {})
+    - data - `dict` - Server data and configs. (Defaults to {})
     """
 
     serverId: str
 
     prefix: Optional[str] = None
+
+    logging: dict = {}
+
+    members: dict = {}
+
+    data: dict = {}
